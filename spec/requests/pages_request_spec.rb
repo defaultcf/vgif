0976c0d5022bab34d3603b79422c57015dd8dd7b
i544c <leo@i544c.me>
2020-03-26 00:47:38 +0900

    Add FactoryBot

--- spec/requests/pages_request_spec.rb
+++ spec/requests/pages_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
   it 'this is private page' do
-    user = User.create(email: 'user@example.com', password: 'password')
+    user = create(:user, email: 'test@example.com')
     sign_in user
     get root_path
     expect(response).to render_template('pages/top')
