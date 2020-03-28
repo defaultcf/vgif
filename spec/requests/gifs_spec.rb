0bcbf6d8c5cb8110b585a50e5b0cd037e17ac72f
i544c <leo@i544c.me>
2020-03-28 21:10:29 +0900

    Fix spec

--- spec/requests/gifs_spec.rb
+++ spec/requests/gifs_spec.rb
@@ -20,6 +20,11 @@ RSpec.describe "/gifs", type: :request do
   end
 
   describe "GET /new" do
+    it 'when not login, redirect login page' do
+      get new_gif_url
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
     it "renders a successful response" do
       sign_in user
       get new_gif_url
@@ -28,15 +33,33 @@ RSpec.describe "/gifs", type: :request do
   end
 
   describe "GET /edit" do
+    it 'when not login, redirect login page' do
+      gif = create(:gif, user: user)
+      get edit_gif_url(gif)
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
     it "render a successful response" do
       sign_in user
       gif = create(:gif, user: user)
       get edit_gif_url(gif)
       expect(response).to be_successful
     end
+
+    it 'error when wrong user' do
+      sign_in create(:user, email: 'user2@example.com')
+      gif = create(:gif, user: user)
+      get edit_gif_url(gif)
+      expect(response).to render_template('errors/403')
+    end
   end
 
   describe "POST /create" do
+    it 'when not login, redirect login page' do
+      post gifs_url, params: { gif: attributes_for(:gif) }
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
     context "with valid parameters" do
       before do
         sign_in user
@@ -73,6 +96,12 @@ RSpec.describe "/gifs", type: :request do
   end
 
   describe "PATCH /update" do
+    it 'when not login, redirect login page' do
+      gif = create(:gif, user: user)
+      patch gif_url(gif), params: { gif: { title: 'aaa' } }
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
     context "with valid parameters" do
       let(:new_attributes) {
         {
@@ -108,24 +137,47 @@ RSpec.describe "/gifs", type: :request do
         expect(response).to be_successful
       end
     end
+
+    it 'error when wrong user' do
+      sign_in create(:user, email: 'user2@example.com')
+      gif = create(:gif, user: user)
+      patch gif_url(gif), params: { gif: { title: 'aaa' } }
+      expect(response).to render_template('errors/403')
+    end
   end
 
   describe "DELETE /destroy" do
-    before do
-      sign_in user
-    end
-
-    it "destroys the requested gif" do
-      gif = create(:gif, user: user)
-      expect {
-        delete gif_url(gif)
-      }.to change(Gif, :count).by(-1)
-    end
-
-    it "redirects to the gifs list" do
+    it 'when not login, redirect login page' do
       gif = create(:gif, user: user)
       delete gif_url(gif)
-      expect(response).to redirect_to(gifs_url)
+      expect(response).to redirect_to(new_user_session_path)
     end
+
+    context 'when login' do
+      before do
+        sign_in user
+      end
+
+      it "destroys the requested gif" do
+        gif = create(:gif, user: user)
+        expect {
+          delete gif_url(gif)
+        }.to change(Gif, :count).by(-1)
+      end
+
+      it "redirects to the gifs list" do
+        gif = create(:gif, user: user)
+        delete gif_url(gif)
+        expect(response).to redirect_to(gifs_url)
+      end
+    end
+
+    it 'error when wrong user' do
+      sign_in create(:user, email: 'user2@example.com')
+      gif = create(:gif, user: user)
+      delete gif_url(gif)
+      expect(response).to render_template('errors/403')
+    end
+
   end
 end
