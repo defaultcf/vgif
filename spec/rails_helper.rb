166617016b9b22ee99bb4d31e18338cd2ebe70c6
i544c <leo@i544c.me>
2020-04-15 21:59:21 +0900

    Add test using Selenium

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,5 +64,5 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :request
-  config.include Devise::Test::IntegrationHelpers, type: :system
+  config.include Warden::Test::Helpers
 end
--- spec/system/gifs_spec.rb
+++ spec/system/gifs_spec.rb
@@ -1,17 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "Gifs", type: :system do
+RSpec.describe "Gifs", type: :system, js: true do
   let(:user) { create(:user, email: 'test@example.com') }
 
-  before do
-    driven_by(:rack_test)
-  end
-
   it 'create gif' do
-    sign_in user
-    visit '/gifs/new'
-    page.attach_file('gif_image', Rails.root.join('spec', 'factories', 'images', 'lulu_wink.gif'))
-    click_button '登録する'
-    expect(page).to have_text('New gif')
+    login_as user, scope: :user
+    visit new_gif_path
+    expect(page).to have_text('登録する')
   end
 end
