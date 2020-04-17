c6c6f8965fa555b25ec8784a09c527c4a1d44fe7
i544c <leo@i544c.me>
2020-04-18 08:10:37 +0900

    Add username and displayname to users

---
+++ db/migrate/20200417221629_add_name_to_user.rb
@@ -0,0 +1,24 @@
+class AddNameToUser < ActiveRecord::Migration[6.0]
+  def up
+    add_column :users, :username, :string, null: false, default: ''
+    add_column :users, :displayname, :string, null: false, default: ''
+
+    execute <<~SQL
+      UPDATE users
+      SET
+        username = mapping.new_username,
+        displayname = mapping.new_username
+      FROM (
+        SELECT
+          id AS new_username
+        FROM users
+      ) AS mapping
+      WHERE users.id = mapping.new_username;
+    SQL
+  end
+
+  def down
+    remove_column :users, :username
+    remove_column :users, :displayname
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_072516) do
+ActiveRecord::Schema.define(version: 2020_04_17_221629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -84,6 +84,8 @@ ActiveRecord::Schema.define(version: 2020_03_29_072516) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username", default: "", null: false
+    t.string "displayname", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
