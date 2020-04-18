4bc771c929adf7bf5d0d69f6e70f607526e50bf0
i544c <leo@i544c.me>
2020-04-19 06:23:31 +0900

    Add test for user page

--- spec/routing/user_routing_spec.rb
+++ spec/routing/user_routing_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe User::InfoController, type: :routing do
     it 'routes to #show' do
       expect(get: '/@i544c').to route_to('user/info#show', username: 'i544c')
     end
+
+    it 'routes to #edit' do
+      expect(get: '/user/edit').to route_to('user/info#edit')
+    end
+
+    it 'routes to #update' do
+      expect(patch: '/user/update').to route_to('user/info#update')
+    end
   end
 end
