2177723293d2089cb95973ce45f0298baebe2232
i544c <leo@i544c.me>
2020-04-20 00:26:34 +0900

    Add users system spec

---
+++ spec/system/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'User', type: :system, js: true do
+  let(:user) { create(:user, username: 'old_username', displayname: 'old_displayname') }
+
+  context 'modify info' do
+    before do
+      login_as user, scope: :user
+      visit user_edit_path
+    end
+
+    it 'success to modify' do
+      fill_in 'user[username]', with: 'asacoco'
+      fill_in 'user[displayname]', with: '朝ココ'
+      click_button '更新する'
+      expect(page).to have_text('successfully')
+      expect(page).to have_text('asacoco')
+      expect(page).to have_text('朝ココ')
+    end
+  end
+end
