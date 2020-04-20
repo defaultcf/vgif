10961b31a9b59b7c66e7793bb5ecf20937ccd14f
i544c <leo@i544c.me>
2020-04-20 23:02:13 +0900

    Fetch meta data

--- app/javascript/packs/gif_form.js
+++ app/javascript/packs/gif_form.js
@@ -10,15 +10,22 @@ file_field.addEventListener('change', input => {
     reader.readAsDataURL(input.target.files[0]);
   }
 });
+
 const remote_url_field = document.querySelector('input[name="gif[remote_image_url]"]');
+const source_url_field = document.querySelector('input[name="gif[source_url]"]');
 let timer;
 remote_url_field.addEventListener('input', e => {
   window.clearTimeout(timer);
-  timer = window.setTimeout(() => {
-    if (remote_url_field.value === '') return;
+  timer = window.setTimeout(async() => {
+    const gifs_id = remote_url_field.value.match(/\/([\w-_]+).gif$/);
+    if (!gifs_id) return;
     preview.setAttribute('src', remote_url_field.value);
+    const res = await fetch(`/api/gifscom/get_meta.json?gifs_id=${gifs_id[1]}`);
+    const json = await res.json();
+    source_url_field.value = `https://youtu.be/${json.yid}?t=${Math.trunc(json.start)}`;
   }, 3000);
 });
+
 preview.addEventListener('click', () => {
   file_field.click();
 });
