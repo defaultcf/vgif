3e8dca4c3a15a67bb929ffec457bf13951202ffe
i544c <leo@i544c.me>
2020-04-22 08:07:40 +0900

    Fix fetch image from S3

--- app/javascript/packs/share.js
+++ app/javascript/packs/share.js
@@ -1,23 +1,24 @@
 const main = async () => {
-  const gif_dom = document.querySelector('#gif');
-  if (!gif_dom) return;
+  const direct_url = document.querySelector('#direct_url');
+  if (!direct_url) return;
 
   const share_button = document.querySelector('#share-button');
+  share_button.setAttribute('disabled', true);
   if (!navigator.share) {
     share_button.style.display = 'none';
     return;
   }
 
   const title = document.querySelector('.title').innerText;
-  const res = await fetch(gif_dom.src);
+  const res = await fetch(direct_url.value, { mode: 'cors' });
   const blob = await res.blob();
   const file = new File([blob], 'file.gif', { type: 'image/gif' });
+  share_button.removeAttribute('disabled');
 
   share_button.addEventListener('click', () => {
     try {
       navigator.share({
-        text: title,
-        url: location.href,
+        text: `${title} ${location.href}`,
         files: [file],
       });
     } catch (e) {
