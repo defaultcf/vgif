44a8b96c4a415b8bd2bf844000e4989938cc89c3
i544c <leo@i544c.me>
2020-05-02 15:10:42 +0900

    Modify migration for rubocop

--- db/migrate/20200329072516_add_missing_indexes_on_taggings.acts_as_taggable_on_engine.rb
+++ db/migrate/20200329072516_add_missing_indexes_on_taggings.acts_as_taggable_on_engine.rb
@@ -28,8 +28,8 @@ AddMissingIndexesOnTaggings.class_eval do
       add_index ActsAsTaggableOn.taggings_table, %i[tagger_id tagger_type]
     end
 
-    unless index_exists? ActsAsTaggableOn.taggings_table, %i[taggable_id taggable_type tagger_id context], name: 'taggings_idy'
-      add_index ActsAsTaggableOn.taggings_table, %i[taggable_id taggable_type tagger_id context], name: 'taggings_idy'
-    end
+    return if index_exists? ActsAsTaggableOn.taggings_table, %i[taggable_id taggable_type tagger_id context], name: 'taggings_idy'
+
+    add_index ActsAsTaggableOn.taggings_table, %i[taggable_id taggable_type tagger_id context], name: 'taggings_idy'
   end
 end
