bc614a5a517f3617f856a743fa003756eb261874
i544c <leo@i544c.me>
2020-05-03 02:56:25 +0900

    Fix for rubocop

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Raven.configure do |config|
   config.dsn = Rails.application.credentials.raven[:dsn]
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
