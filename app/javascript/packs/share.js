0ef5d7f621097671cf3c8657b16544b50f1db7b5
i544c <leo@i544c.me>
2020-05-07 20:27:05 +0900

    Add tags to sharing text

--- app/javascript/packs/share.js
+++ app/javascript/packs/share.js
@@ -2,6 +2,12 @@ const main = async () => {
   const direct_url = document.querySelector('#direct_url');
   if (!direct_url) return;
 
+  const title = document.querySelector('.title').innerText;
+  const tags = Array
+    .from(document.querySelectorAll('span.tag'))
+    .map(tag => tag.innerText)
+    .join(' ');
+
   const share_button = document.querySelector('#share-button');
   share_button.setAttribute('disabled', true);
   if (!navigator.share) {
@@ -9,7 +15,6 @@ const main = async () => {
     return;
   }
 
-  const title = document.querySelector('.title').innerText;
   const res = await fetch(direct_url.value, { mode: 'cors' });
   const blob = await res.blob();
   const file = new File([blob], 'file.gif', { type: 'image/gif' });
@@ -18,7 +23,7 @@ const main = async () => {
   share_button.addEventListener('click', () => {
     try {
       navigator.share({
-        text: `${title} ${location.href}`,
+        text: `${title} ${location.href} ${tags}`,
         files: [file],
       });
     } catch (e) {
